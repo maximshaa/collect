9ee0399c70b60beab4700ffec1bceb24830e1194
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2025-05-11 21:06:21 +0700

    done

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'name' => 'mironova2056/collect',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c55423b31b612d87fa69415ce4b65e3b1c4ac4b3',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+        'mironova2056/collect' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c55423b31b612d87fa69415ce4b65e3b1c4ac4b3',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
